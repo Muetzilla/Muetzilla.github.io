28ae366bd81914df5ddf41f8c14f6476607ee81e
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-04-27 13:19:43 +0200

    wuhu working

--- javaScript/learningCanvas.js
+++ javaScript/learningCanvas.js
@@ -5,3 +5,15 @@ function draw() {
       var ctx = canvas.getContext('2d');
     }
   }
+  function drawRectangles(){
+    var canvasRectangles = document.getElementById('canvasRectagles');
+    if (canvasRectangles.getContext) {
+        var canvasRec = canvasRectangles.getContext('2d');
+
+        canvasRec.fillStyle = 'rgb(200, 0, 0)';
+        canvasRec.fillRect(10, 10, 50, 50);
+
+        canvasRec.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        canvasRec.fillRect(30, 30, 50, 50);    
+      }
+  }
