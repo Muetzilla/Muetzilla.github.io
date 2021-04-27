ff87fc20309e3ab1ef0c1a85e65929b965f5fbbf
Muetzilla <54369574+Muetzilla@users.noreply.github.com>
2021-04-27 13:04:47 +0200

    canvas in external file

---
+++ javaScript/learningCanvas.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',domloaded,false);
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+    }
+  }
